7083c0b9827168b5a8460069062a4158577a8857
Maureen Josephine <Maureen Josephine>
2020-01-28 16:34:35 +0300

    modify books model

--- src/main/java/model/Books.java
+++ src/main/java/model/Books.java
@@ -1,68 +1,93 @@
 package model;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 
 @Entity
     public class Books {
 
-       @Id
-       @GeneratedValue(strategy = GenerationType.AUTO )
-        private Long id;
-        private String title;
-        private String isbn;
-        private String publisher;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isbn;
+    private String publisher;
 
-        @ManyToMany
-        private Set<Authors> authors = new HashSet<>();
-        public  Books(){
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Authors> authors = new HashSet<>();
 
-        }
-        public  Books(String title, String isbn, String publisher){
-            this.title = title;
-            this.isbn = isbn;
-            this.publisher = publisher;
-        }
+    public Books() {
 
-        public  Books(String title, String isbn, String publisher, Set<Authors> authors){
-            this.title = title;
-            this.isbn = isbn;
-            this.publisher = publisher;
-            this.authors =  authors;
-        }
-
-        public String getTitle(){
-            return title;
-        }
-        public  void setTitle(String title){
-            this.title = title;
-        }
-
-        public String getIsbn() {
-            return isbn;
-        }
-        public void setIsbn(String isbn){
-            this.isbn = isbn;
-        }
-        public String getPublisher() {
-            return publisher;
-        }
-        public void setPublisher(String publisher){
-            this.publisher = publisher;
-        }
-
-        public Set<Authors> getAuthors() {
-            return authors;
-        }
-
-        public void setAuthors(Set<Authors> authors) {
-            this.authors = authors;
-        }
     }
+
+    public Books(String title, String isbn, String publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+
+    public Books(String title, String isbn, String publisher, Set<Authors> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Set<Authors> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Authors> authors) {
+        this.authors = authors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Books)) return false;
+        Books books = (Books) o;
+        return Objects.equals(id, books.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
+
 
