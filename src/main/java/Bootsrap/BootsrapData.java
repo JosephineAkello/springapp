1e855488cd69010111fc27518095c2ed940990a5
Maureen Josephine <Maureen Josephine>
2020-03-21 22:31:03 +0300

    create Bootsrap data class

---
+++ src/main/java/Bootsrap/BootsrapData.java
@@ -0,0 +1,13 @@
+package Bootsrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootsrapData implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
