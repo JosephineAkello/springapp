fa3b45bbb3dd71a7a377c1c21f1f4abf88950992
Maureen Josephine <Maureen Josephine>
2020-03-22 01:15:43 +0300

    add bootsrap data

--- src/main/java/Bootsrap/BootsrapData.java
+++ src/main/java/Bootsrap/BootsrapData.java
@@ -1,13 +1,55 @@
 package Bootsrap;
 
+import java.awt.*;
+import java.awt.print.Book;
+import java.util.Optional;
+
+import Repository.AuthorRepository;
+import Repository.BookRepository;
+import model.Authors;
+import model.Books;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Component;
+
+import static jdk.nashorn.internal.runtime.regexp.joni.encoding.CharacterType.S;
 
 @Component
 public class BootsrapData implements CommandLineRunner {
 
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private Object Iterable;
+    private Object Book;
+    private Object Example;
+
+
+    public BootsrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
     @Override
     public void run(String... args) throws Exception {
+
+        Authors maureen = new Authors("Maureen", " Jos");
+        Books ddd = new Books("Domain Driven Design", "123123");
+        maureen.getBooks().add(ddd);
+        ddd.getAuthors().add(maureen);
+
+        authorRepository.save(maureen);
+        bookRepository.save(ddd);
+
+        Authors jos = new Authors("Jos", "Josephine");
+        Books jjj = new Books("know your lang", "120120");
+        jos.getBooks().add(jjj);
+        jjj.getAuthors().add(jos);
+
+        authorRepository.save(jos);
+        bookRepository.save(jjj);
+
+
+
 
     }
 }
