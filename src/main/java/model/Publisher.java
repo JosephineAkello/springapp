2a46173b19994701760efa7ffd4b2195614390ba
Maureen Josephine <Maureen Josephine>
2020-03-23 17:27:03 +0300

    map publisher as entiy

--- src/main/java/model/Publisher.java
+++ src/main/java/model/Publisher.java
@@ -2,12 +2,14 @@ package model;
 
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 
+@Entity
 public class Publisher {
 
     @Id
