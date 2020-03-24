1e35472d0d8f02707a67da72375e3076eb2a2b23
Maureen Josephine <Maureen Josephine>
2020-03-24 22:49:22 +0300

    create author controller

--- src/main/java/com/example/springapp/controllers/AuthorController.java
+++ src/main/java/com/example/springapp/controllers/AuthorController.java
@@ -1,4 +1,13 @@
 package com.example.springapp.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AuthorController {
+    
+    @RequestMapping("/authors")
+    public String getAuthor(){
+        return "authors";
+    }
 }
